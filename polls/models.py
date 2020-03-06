3b2fc9d70ffdfd02c2198b636dfe36df5efce68c
mew101 <mshwss@gmail.com>
2020-03-06 11:44:13 -0500

    fixed models and install app

--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text =
-models.Charfield(max_length=20)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-    qeustion = models.ForeignKey(Question, on_delete=models.CASCADE)
-models.CharFiels(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
